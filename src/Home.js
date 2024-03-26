e95f60c35bea09e5d6058d9648cc647b712bf5c0
GeoT82 <gtinoco354@gmail.com>
2024-03-26 18:11:40 -0400

    Added navigation between home and settings

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <form action="">
+          <textarea name="" id="" cols="10" rows="1"></textarea>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    
+        {/* 
+        <img src={logo} className="Home-logo" alt="logo" />
+            <p>
+              Edit <code>src/Home.js</code> and save to reload.
+            </p>
+            <a
+              className="Home-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+        */}
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default Home;
