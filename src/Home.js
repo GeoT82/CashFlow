1bf3564b7e28a32bea50270b84bb2e8542dbd87d
GeoT82 <gtinoco354@gmail.com>
2024-03-27 23:22:59 -0400

    created basic form to submit money

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,52 @@
 import React from 'react';
+import { FormGroup, FormControl, InputLabel, Input, Button, makeStyles } from '@material-ui/core';
+import { InputAdornment } from '@mui/material';
+
+const useStyle = makeStyles({
+    formStyle: {
+        width: "50%",
+        margin: "auto",
+        padding: 20,
+        border: "1px solid black",
+        paddingTop: 20,
+        boxShadow: "0px 0px 8px rgba(0,0,0,0.5)",
+    },
+    myBtn: {
+        marginTop: 10,
+        width: "10%",
+    }
+});
 
 function Home () {
-
+    const classes = useStyle()
     return(
         <div className="Home">
-        <header className="Home-header">
-            <form action="">
-            <textarea name="" id="" cols="10" rows="1"></textarea>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </header>
+            <header className="Home-header">
+                <h1>Money Transfer</h1>      
+            </header>
+
+            <div className="moneyForm">
+                <FormGroup className={classes.formStyle}>
+                    <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                        <InputLabel htmlFor="contact">Target User</InputLabel>
+                        <Input
+                            id="contact"
+                            startAdornment={<InputAdornment position="start">-</InputAdornment>}
+                        />
+                    </FormControl> 
+                    <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                        <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+                        <Input
+                            id="standard-adornment-amount"
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                        />
+                    </FormControl> 
+                    <Button variant="contained" color='secondary' className={classes.myBtn}>Send</Button> 
+                </FormGroup>
+                
+            </div>
         </div>
+        
     )
     
 }
